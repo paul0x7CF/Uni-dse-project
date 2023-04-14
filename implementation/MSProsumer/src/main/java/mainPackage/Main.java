5727385c97430cc6798fcccd8b35c5d0438704f9
freundp99 <a11919391@unet.univie.ac.at>
2023-04-14 13:40:21 +0200

    resources and logger added

--- implementation/MSProsumer/src/main/java/mainPackage/Main.java
+++ implementation/MSProsumer/src/main/java/mainPackage/Main.java
@@ -1,9 +1,20 @@
 package mainPackage;
 
 import network.NetworkHandler;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Main {
+
+    private static final Logger logger = LogManager.getLogger(Main.class);
     public static void main(String[] args) {
         NetworkHandler network = new NetworkHandler();
+
+        logger.info("Client Started");
+        logger.debug("hello debug");
+        logger.error("hello error");
+        logger.fatal("hello fatal");
+        logger.trace("hello trace");
+        logger.warn("hello warn");
     }
 }
