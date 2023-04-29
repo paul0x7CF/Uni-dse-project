d782a46ec717535ce45a521fd87da0b3d5426c54
freundp99 <a11919391@unet.univie.ac.at>
2023-04-29 21:40:35 +0200

    Prosumer Skeleton

--- implementation/MSProsumer/src/main/java/MSProsumer/Data/SolarPanel.java
+++ implementation/MSProsumer/src/main/java/MSProsumer/Data/SolarPanel.java
@@ -1,4 +1,19 @@
 package MSProsumer.Data;
 
 public class SolarPanel {
+
+    private double area;
+
+    private int efficiency;
+
+    private int compassAngle;
+
+    private int standingAngle;
+
+    public SolarPanel(final double area, final int efficiency, final int compassAngle, final int standingAngle) {
+        this.area = area;
+        this.efficiency = efficiency;
+        this.compassAngle = compassAngle;
+        this.standingAngle = standingAngle;
+    }
 }
--- implementation/MSProsumer/src/main/java/MSProsumer/Logic/Scheduler.java
+++ implementation/MSProsumer/src/main/java/MSProsumer/Logic/Scheduler.java
@@ -1,4 +1,15 @@
 package MSProsumer.Logic;
 
+import MSProsumer.Data.Consumer;
+
+import java.util.List;
+
 public class Scheduler {
+    private int threshold;
+    private List<Consumer> heavyConsumers;
+
+    public Scheduler() {}
+
+    public void addHeavyConsumer(Consumer consumer) {
+    }
 }
