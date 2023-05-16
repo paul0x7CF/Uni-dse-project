dd9e6b273eb13b571bae51af77c9efd3f6ea9342
pflegerm01 <a12014173@unet.univie.ac.at>
2023-05-16 18:11:54 +0200

    optimized code for AuctionManager

--- implementation/MSExchange/src/main/java/msExchange/auctionManagement/AuctionManager.java
+++ implementation/MSExchange/src/main/java/msExchange/auctionManagement/AuctionManager.java
@@ -8,6 +8,7 @@ import sendable.Transaction;
 import java.time.LocalDateTime;
 import java.util.*;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.logging.Logger;
 
 public class AuctionManager implements Runnable {
@@ -15,13 +16,14 @@ public class AuctionManager implements Runnable {
     private final int MINUTES_TO_LIVE_AFTER_EXPIRING = 500000; //in millisecs
     Logger logger = Logger.getLogger(getClass().getName());
     private Map<UUID, Auction> auctions;
-    private Map<UUID, TimeSlot> timeSlots = new HashMap<>();
+    private Map<UUID, TimeSlot> timeSlots;
     private BlockingQueue<Transaction> transactionQueue;
     private BlockingQueue<Bid> bidQueue;
     private BlockingQueue<Sell> sellQueue;
 
     public AuctionManager(final BlockingQueue<Transaction> transactionQueue, final BlockingQueue<Bid> bidQueue, final BlockingQueue<Sell> sellQueue) {
-        this.auctions = new HashMap<>();
+        this.auctions = new ConcurrentHashMap<>();
+        this.timeSlots = new ConcurrentHashMap<>();
         this.transactionQueue = transactionQueue;
         this.bidQueue = bidQueue;
         this.sellQueue = sellQueue;
@@ -73,6 +75,27 @@ public class AuctionManager implements Runnable {
         processSellQueue();
 
         //Process bidQueue
+        processBidQueue();
+
+
+        //Check for ended auctions
+        checkForEndedAuctions();
+
+    }
+
+    private void checkForEndedAuctions() {
+        List<UUID> auctionsToRemove = new ArrayList<>();
+        for (Auction auction : auctions.values()) {
+            if (auction.isAuctionEnded()) {
+                auctionsToRemove.add(auction.getAuctionId());
+            }
+        }
+        for (UUID auctionId : auctionsToRemove) {
+            auctions.remove(auctionId);
+        }
+    }
+
+    private void processBidQueue() throws AuctionNotFoundException, InterruptedException {
         Bid bid = bidQueue.poll(); // Non-blocking call, retrieves the next bid or null if empty
         if (bid != null) {
             Optional<UUID> auctionID = bid.getAuctionID();
@@ -87,25 +110,13 @@ public class AuctionManager implements Runnable {
                 addBidToAuction(auctionID.get(), bid);
             }
         }
-
-
-        //Check for ended auctions
-        Date currentTime = new Date();
-        for (Auction auction : auctions.values()) {
-            if (auction.isAuctionEnded()) {
-                //Remove auction
-                auctions.remove(auction.getAuctionId());
-            }
-        }
-
     }
 
     private void processSellQueue() throws AuctionNotFoundException {
         Sell sell = sellQueue.poll(); // Non-blocking call, retrieves the next sell or null if empty
         if (sell != null) {
             TimeSlot timeSlot = timeSlots.get(sell.getTimeSlot());
-            String uuid = "11a1-22a2-33a3-44a4-55a5";
-            if (timeSlot.getEndTime().isBefore(LocalDateTime.now()) && timeSlot.getTimeSlotId() != UUID.fromString(uuid)) {
+            if (timeSlot.getEndTime().isBefore(LocalDateTime.now())) {
                 //endTime is in the past
                 //send Message to Storage -> Message Builder!
             } else {
@@ -116,10 +127,7 @@ public class AuctionManager implements Runnable {
     }
 
     private boolean auctionExists(UUID auctionID) {
-        if (auctions.get(auctionID) == null) {
-            return false;
-        }
-        return true;
+        return auctions.containsKey(auctionID);
     }
 
     private void addNewAuction(Sell sell) throws AuctionNotFoundException {
@@ -134,8 +142,8 @@ public class AuctionManager implements Runnable {
 
     private void addBidToAuction(UUID auctionID, Bid bid) {
         TimeSlot timeSlot = timeSlots.get(bid.getTimeSlot());
-        String uuid = "11a1-22a2-33a3-44a4-55a5";
-        if (timeSlot.getEndTime().isBefore(LocalDateTime.now()) && timeSlot.getTimeSlotId() != UUID.fromString(uuid)) {
+
+        if (timeSlot.getEndTime().isBefore(LocalDateTime.now())) {
             //endTime is in the past
             logger.info("Bid was for a previous slot. Sending the Bid to the Storage...");
             //send Message to Storage -> Message Builder!
@@ -155,7 +163,7 @@ public class AuctionManager implements Runnable {
     }
 
     public Map<UUID, Auction> getAuctions() {
-        return auctions;
+        return Collections.unmodifiableMap(auctions);
     }
 
     public void addTimeSlots(List<sendable.TimeSlot> newTimeSlots) {
