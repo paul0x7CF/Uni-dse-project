ac05705d7499beeca86174d4bbc473b08c23dd03
freundp99 <a11919391@unet.univie.ac.at>
2023-05-27 18:39:00 +0200

    Constructor logic added fail to initialize BrokerRunner

--- implementation/MSProsumer/src/main/java/Communication/Communication.java
+++ implementation/MSProsumer/src/main/java/Communication/Communication.java
@@ -1,25 +1,31 @@
 package Communication;
 
+import Logic.Prosumer;
 import MSProsumer.Main.ProsumerManager;
-import broker.Broker;
+import broker.BrokerRunner;
 import messageHandling.MessageHandler;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import protocol.Message;
+import sendable.EServiceType;
 import sendable.MSData;
 import sendable.TimeSlot;
 
 import java.util.concurrent.BlockingQueue;
 
 public class Communication {
+
+    private static final Logger logger = LogManager.getLogger(Communication.class);
     private int port;
     private String ipAdress;
 
-    private MSData myData;
+    private MSData myMSData;
 
-    private Broker communicationBroker;
+    private BrokerRunner communicationBroker;
 
     private BlockingQueue<TimeSlot> inputQueueTimeSlot;
 
-    private BlockingQueue<Message> inputForecastResponse;
+    private BlockingQueue<Message> incomingMessages;
 
     private BlockingQueue<Message> outgoingMessages;
 
@@ -29,9 +35,17 @@ public class Communication {
 
     public Communication(BlockingQueue<TimeSlot> inputQueueTimeSlot, BlockingQueue<Message> inputForecastResponse, BlockingQueue<Message> outgoingMessages, ProsumerManager prosumerManager) {
         this.inputQueueTimeSlot = inputQueueTimeSlot;
-        this.inputForecastResponse = inputForecastResponse;
+        //this.inputForecastResponse = inputForecastResponse;
         this.outgoingMessages = outgoingMessages;
         this.prosumerManager = prosumerManager;
+    }
+
+    public Communication(BlockingQueue<Message> incomingMessages, BlockingQueue<Message> outgoingMessages) {
+        this.incomingMessages = outgoingMessages;
+        this.outgoingMessages = outgoingMessages;
+        this.communicationBroker = new BrokerRunner(EServiceType.Prosumer,8888);
+        this.myMSData = this.communicationBroker.getCurrentService();
+        logger.info("MS registered with Id: {} Ip: {} Port: {}", this.myMSData.getId(), this.myMSData.getAddress(), this.myMSData.getPort());
     }
 
     private void createBroker() {
