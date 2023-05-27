aa89184e88de0b248b8cf2e4b0233af6ed33c69c
freundp99 <a11919391@unet.univie.ac.at>
2023-05-27 22:45:39 +0200

    logic for ConfigFile moved to ConfigFileReader Class

--- implementation/MSProsumer/src/main/java/MSProsumer/Main/Main.java
+++ implementation/MSProsumer/src/main/java/MSProsumer/Main/Main.java
@@ -1,7 +1,9 @@
 package MSProsumer.Main;
 
+import Configuration.ConfigFileReader;
 import Data.EProsumerType;
 import Logic.Prosumer;
+import mainPackage.ConfigReader;
 import mainPackage.MainForTesting;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -9,34 +11,18 @@ import org.apache.logging.log4j.Logger;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Optional;
 import java.util.Properties;
 import java.util.Random;
 
 public class Main {
 
-    private static final Logger logger = LogManager.getLogger(MainForTesting.class);
+    private static final Logger logger = LogManager.getLogger(Main.class);
     public static void main(String[] args) {
 
         //read in the config file
-        final int PROSUMER_AMOUNT;
-        final double CASH_BALANCE;
-
-        try {
-            Properties properties = new Properties();
-            final String PATH = "implementation/MSProsumer/src/main/resources/config.properties";
-            FileInputStream configFile = new FileInputStream(PATH);
-            properties.load(configFile);
-
-            PROSUMER_AMOUNT = Integer.parseInt(properties.getProperty("prosumer.amount"));
-            CASH_BALANCE = Double.parseDouble(properties.getProperty("wallet.cashBalance"));
-
-        } catch (FileNotFoundException e) {
-            logger.error("Config file not found");
-            throw new RuntimeException(e);
-        } catch (IOException e) {
-            logger.error("Error while reading config file");
-            throw new RuntimeException(e);
-        }
+        final int PROSUMER_AMOUNT = Integer.parseInt(ConfigFileReader.getProperty("prosumer.amount"));
+        final double CASH_BALANCE = Double.parseDouble(ConfigFileReader.getProperty("wallet.cashBalance"));
 
 
         for (int i = 0; i < PROSUMER_AMOUNT; i++) {
