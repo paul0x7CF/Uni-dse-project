33c97a04009637505ea8db689a61f730d365b44b
freundp99 <a11919391@unet.univie.ac.at>
2023-05-27 22:43:26 +0200

    ConfigFileReader for Communication bug fixed

--- implementation/Communication/src/main/java/mainPackage/ConfigReader.java
+++ implementation/Communication/src/main/java/mainPackage/ConfigReader.java
@@ -1,16 +1,23 @@
 package mainPackage;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.FileInputStream;
 import java.io.InputStream;
 import java.util.Properties;
 
 public class ConfigReader {
-    private static final String PROPERTIES_FILE = "config.properties";
+
+    private static final Logger logger = LogManager.getLogger(ConfigReader.class);
+    private static final String PROPERTIES_FILE = "implementation/Communication/src/main/resources/config.properties";
     private final Properties properties;
 
     public ConfigReader() {
-        try (InputStream input = getClass().getClassLoader().getResourceAsStream(PROPERTIES_FILE)) {
+        try {
             properties = new Properties();
-            properties.load(input);
+            FileInputStream configFile = new FileInputStream(PROPERTIES_FILE);
+            properties.load(configFile);
         } catch (Exception e) {
             throw new RuntimeException("Error loading properties file", e);
         }
