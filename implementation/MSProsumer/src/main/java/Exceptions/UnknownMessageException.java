6248e3833d07b569b2c2c07b58acedf5e3e462d8
freundp99 <a11919391@unet.univie.ac.at>
2023-05-28 15:29:34 +0200

    Exception added

---
+++ implementation/MSProsumer/src/main/java/Exceptions/UnknownMessageException.java
@@ -0,0 +1,7 @@
+package Exceptions;
+
+public class UnknownMessageException extends Exception{
+    public UnknownMessageException() {
+        super("Message was Ignored because it was unknown");
+    }
+}
