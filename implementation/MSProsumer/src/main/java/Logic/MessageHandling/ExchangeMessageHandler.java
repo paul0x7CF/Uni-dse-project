d0c8f3b00c6b57b1911418fbfb5d4f50ab6e1e27
freundp99 <a11919391@unet.univie.ac.at>
2023-05-29 17:53:45 +0200

    Prosumer MessageHandlers added

--- implementation/MSProsumer/src/main/java/Logic/MessageHandling/ExchangeMessageHandler.java
+++ implementation/MSProsumer/src/main/java/Logic/MessageHandling/ExchangeMessageHandler.java
@@ -1,13 +1,32 @@
 package Logic.MessageHandling;
 
+import Exceptions.MessageNotSupportedException;
+import Logic.Prosumer.Prosumer;
 import exceptions.MessageProcessingException;
 import exceptions.RemoteException;
 import messageHandling.IMessageHandler;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import protocol.Message;
 
 public class ExchangeMessageHandler implements IMessageHandler {
+
+    private static final Logger logger = LogManager.getLogger(ExchangeMessageHandler.class);
+
     @Override
     public void handleMessage(Message message) throws MessageProcessingException, RemoteException {
+        try {
+            switch (message.getSubCategory()) {
+                case "TimeSlot" -> handleTimeSlot(message);
+                default -> throw new MessageNotSupportedException();
+            }
+        } catch (MessageNotSupportedException e) {
+            logger.warn(e.getMessage());
+        }
 
+    }
+
+    private void handleTimeSlot(Message message) {
+        logger.trace("TimeSlot message received");
     }
 }
