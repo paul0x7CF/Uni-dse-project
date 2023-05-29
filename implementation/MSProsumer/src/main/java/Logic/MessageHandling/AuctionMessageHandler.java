ce5f1568c139b637b1bcba89836ec91a9395c1e1
freundp99 <a11919391@unet.univie.ac.at>
2023-05-29 16:56:14 +0200

    added Logic for AuctionMessageHandler to differ BidHigher and SellLower

--- implementation/MSProsumer/src/main/java/Logic/MessageHandling/AuctionMessageHandler.java
+++ implementation/MSProsumer/src/main/java/Logic/MessageHandling/AuctionMessageHandler.java
@@ -9,10 +9,26 @@ import protocol.Message;
 
 public class AuctionMessageHandler implements IMessageHandler {
 
-    private IBroker broker;;
+    private IBroker broker;
     private Prosumer prosumer;
     @Override
     public void handleMessage(Message message) throws MessageProcessingException, RemoteException {
-
+       /* Bid, 
+        Sell, 
+        Transaction, 
+        TimeSlot*/
+        switch (message.getSubCategory()) {
+            case "BidHigher" -> handleBidHigher(message);
+            case "SellLower" -> handleSellLower(message);
+            
+        }
     }
+
+    private void handleSellLower(Message message) {
+    }
+
+    private void handleBidHigher(Message message) {
+    }
+
+
 }
