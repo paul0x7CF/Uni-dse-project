143dc783fbe89d87533d4e8eff419859587009d5
Günther Dobler <a11911466@univie.ac.at>
2023-05-29 19:15:47 +0200

    extended ack for more fields

--- implementation/Communication/src/main/java/sendable/AckInfo.java
+++ implementation/Communication/src/main/java/sendable/AckInfo.java
@@ -7,10 +7,14 @@ import java.util.UUID;
 public class AckInfo implements ISendable {
     private final UUID messageID;
     private final String category;
+    private final int senderPort;
+    private final int receiverPort;
 
-    public AckInfo(UUID messageID, String category) {
+    public AckInfo(UUID messageID, String category, int senderPort, int receiverPort) {
         this.messageID = messageID;
         this.category = category;
+        this.senderPort = senderPort;
+        this.receiverPort = receiverPort;
     }
 
     public UUID getMessageID() {
@@ -19,5 +23,13 @@ public class AckInfo implements ISendable {
 
     public String getCategory() {
         return category;
+    }
+
+    public int getSenderPort() {
+        return senderPort;
+    }
+
+    public int getReceiverPort() {
+        return receiverPort;
     }
 }
