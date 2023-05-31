4fc456c389f136a1f59a7a6561ec93c33e8ed4b0
pflegerm01 <a12014173@unet.univie.ac.at>
2023-05-31 16:58:46 +0200

    updated bidValidator

--- implementation/MSExchange/src/main/java/msExchange/messageHandling/BidValidator.java
+++ implementation/MSExchange/src/main/java/msExchange/messageHandling/BidValidator.java
@@ -5,17 +5,16 @@ import sendable.Bid;
 
 public class BidValidator {
     public static void checkBid(Bid bid) throws InvalidBidException {
-        if(bid == null){
-            throw new InvalidBidException("Bid is null");
+        if (bid == null) {
+            throw new InvalidBidException("Bid is null", null);
         }
-        if(bid.getBidderID() == null){
-            throw new InvalidBidException("BidderID is null");
+        if (bid.getBidderID() == null) {
+            throw new InvalidBidException("BidderID is null", null);
         }
         //TODO: check if BidderID same as serviceID
-        if(bid.getPrice() < 0){
-            throw new InvalidBidException("Price is null");
+        if (bid.getPrice() < 0) {
+            throw new InvalidBidException("Price is null", bid.getBidderID());
         }
-
 
 
     }
