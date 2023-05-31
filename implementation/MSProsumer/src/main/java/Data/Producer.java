57521550081a2cadfa89341fb18df49b3d6628e2
freundp99 <a11919391@unet.univie.ac.at>
2023-05-31 13:14:30 +0200

    Producer and IProsumerDevice added

---
+++ implementation/MSProsumer/src/main/java/Data/Producer.java
@@ -0,0 +1,12 @@
+package Data;
+
+public class Producer implements IProsumerDevice{
+
+    private double area;
+
+    private int efficiency;
+
+    private int compassAngle;
+
+    private int standingAngle;
+}
