4211a40760b24d22f1e217ecf340ecd35d82136a
freundp99 <a11919391@unet.univie.ac.at>
2023-05-31 13:01:01 +0200

    Accounting Strategy a bit logic added

--- implementation/MSProsumer/src/main/java/Logic/AccountingStrategy/CalcConsumption.java
+++ implementation/MSProsumer/src/main/java/Logic/AccountingStrategy/CalcConsumption.java
@@ -1,8 +1,13 @@
 package Logic.AccountingStrategy;
 
+import Data.IProsumerDevice;
+
+import java.util.List;
+
 public class CalcConsumption implements ICalAcctStrategy {
     @Override
-    public void calculateAccounting() {
+    public double calculateAccounting(List<IProsumerDevice> devices) {
 
+        return 0;
     }
 }
