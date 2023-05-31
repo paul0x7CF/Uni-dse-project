4af8b60f49707bea36fc1efd3520f52a732dc478
pflegerm01 <a12014173@unet.univie.ac.at>
2023-05-31 22:49:50 +0200

    updated bidValidator

--- implementation/MSExchange/src/main/java/msExchange/messageHandling/BidValidator.java
+++ implementation/MSExchange/src/main/java/msExchange/messageHandling/BidValidator.java
@@ -3,30 +3,54 @@ package msExchange.messageHandling;
 import msExchange.Exceptions.InvalidBidException;
 import sendable.Bid;
 
-public class BidValidator {
+import java.util.Optional;
+import java.util.UUID;
 
-    public void blabla(){
-        try {
-            checkBid(null);
-        } catch (InvalidBidException e) {
-            e.getBidderID();
-            throw new RuntimeException(e);
-        }
+public final class BidValidator {
+
+    public static void validateBid(Bid bid) throws InvalidBidException {
+        validateBidNotNull(bid);
+        validateBidderIDNotNull(bid.getBidderID());
+        validatePriceNotNegative(bid.getPrice(), bid.getBidderID());
+        validateVolumeNotNegative(bid.getVolume(), bid.getBidderID());
+        validateAuctionIDNotNull(bid.getAuctionID(), bid.getBidderID());
+        validateTimeSlotNotNull(bid.getTimeSlot(), bid.getBidderID());
     }
 
-    public static void checkBid(Bid bid) throws InvalidBidException {
+    private static void validateBidNotNull(Bid bid) throws InvalidBidException {
         if (bid == null) {
             throw new InvalidBidException("Bid is null", null);
         }
-        if (bid.getBidderID() == null) {
+    }
+
+    private static void validateBidderIDNotNull(UUID bidderID) throws InvalidBidException {
+        if (bidderID == null) {
             throw new InvalidBidException("BidderID is null", null);
         }
-        //TODO: check if BidderID same as serviceID
-        if (bid.getPrice() < 0) {
-            throw new InvalidBidException("Price is null", bid.getBidderID());
+    }
+
+    private static void validatePriceNotNegative(double price, UUID bidderID) throws InvalidBidException {
+        if (price <= 0) {
+            throw new InvalidBidException("Price is smaller than zero", bidderID);
         }
+    }
 
+    private static void validateVolumeNotNegative(double volume, UUID bidderID) throws InvalidBidException {
+        if (volume <= 0) {
+            throw new InvalidBidException("Volume is smaller than zero", bidderID);
+        }
+    }
 
+    private static void validateAuctionIDNotNull(Optional<UUID> auctionID, UUID bidderID) throws InvalidBidException {
+        if (auctionID.isEmpty()) {
+            throw new InvalidBidException("AuctionID is null", bidderID);
+        }
+    }
+
+    private static void validateTimeSlotNotNull(UUID timeSlot, UUID bidderID) throws InvalidBidException {
+        if (timeSlot == null) {
+            throw new InvalidBidException("TimeSlot is null", bidderID);
+        }
     }
 
 }
