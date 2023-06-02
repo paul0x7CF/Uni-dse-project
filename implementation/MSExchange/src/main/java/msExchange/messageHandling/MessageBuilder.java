36410827031a15ddfe400489feb5d67f57aa91c4
pflegerm01 <a12014173@unet.univie.ac.at>
2023-06-02 12:54:08 +0200

    added logic to send transaction

--- implementation/MSExchange/src/main/java/msExchange/messageHandling/MessageBuilder.java
+++ implementation/MSExchange/src/main/java/msExchange/messageHandling/MessageBuilder.java
@@ -2,8 +2,16 @@ package msExchange.messageHandling;
 
 import broker.BrokerRunner;
 import exceptions.MessageProcessingException;
+import mainPackage.ESubCategory;
+import protocol.ECategory;
 import protocol.Message;
+import protocol.MessageFactory;
+import sendable.EServiceType;
+import sendable.MSData;
 import sendable.Transaction;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MessageBuilder {
     private BrokerRunner broker;
@@ -20,8 +28,37 @@ public class MessageBuilder {
         //TODO: implement
     }
 
-    public Message buildMessage(Transaction transaction) {
-        //TODO: implement
-        return null;
+    public List<Message> buildMessage(Transaction transaction) {
+        List<Message> messages = new ArrayList<>();
+        List<MSData> receiverMS = new ArrayList<>();
+
+        receiverMS.add(broker.findService(transaction.getSellerID()));
+        receiverMS.add(broker.findService(transaction.getBuyerID()));
+
+        for (MSData msData : broker.getServicesByType(EServiceType.Storage)) {
+            receiverMS.add(msData);
+        }
+
+        for (MSData msData : receiverMS) {
+            messages.add(buildTransactionMessage(senderAndReceiverTemplate(msData), transaction));
+        }
+
+        return messages;
+    }
+
+    private Message buildTransactionMessage(MessageFactory messageFactory, Transaction transaction) {
+        messageFactory.setCategory(ECategory.Auction, String.valueOf(ESubCategory.Transaction)).setPayload(transaction);
+        return messageFactory.build();
+    }
+
+    public MessageFactory senderAndReceiverTemplate(MSData reciever) {
+        MessageFactory messageFactory = new MessageFactory();
+        MSData sender = broker.getCurrentService();
+        return messageFactory.setSenderID(sender.getId())
+                .setSenderAddress(sender.getAddress())
+                .setSenderPort(sender.getPort())
+                .setReceiverID(reciever.getId())
+                .setReceiverAddress(reciever.getAddress())
+                .setReceiverPort(reciever.getPort());
     }
 }
