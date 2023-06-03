f69b60ca4c038ef749938367a6b7f6a447e6603d
freundp99 <a11919391@unet.univie.ac.at>
2023-06-03 15:15:07 +0200

    MessageBuilder created and logic to build ConsumptionForecast Message

--- implementation/MSProsumer/src/main/java/Communication/MessageHandling/MessageBuilder.java
+++ implementation/MSProsumer/src/main/java/Communication/MessageHandling/MessageBuilder.java
@@ -1,4 +1,38 @@
 package Communication.MessageHandling;
 
+import protocol.ECategory;
+import protocol.Message;
+import protocol.MessageFactory;
+import sendable.ConsumptionRequest;
+import sendable.MSData;
+
 public class MessageBuilder {
+
+    private MSData sender;
+
+    public MessageBuilder() {
+        //TODO: implement constructor
+        //Maybe add broker here?
+        //Maybe only some types of broker?
+    }
+
+    public Message buildConsumptionForecastMessage(ConsumptionRequest consumptionRequest, MSData receiver){
+        //TODO: implement logic to build consumption forecast message
+        MessageFactory consumptionMessageFactory = senderAndReceiverTemplate(receiver);
+        consumptionMessageFactory.
+                setCategory(ECategory.Forecast, "Consumption").
+                setPayload(consumptionRequest);
+        return consumptionMessageFactory.build();
+    }
+
+
+    private MessageFactory senderAndReceiverTemplate(MSData receiver) {
+        MessageFactory messageFactory = new MessageFactory();
+        return messageFactory.setSenderID(sender.getId()).
+                setSenderAddress(sender.getAddress()).
+                setSenderPort(sender.getPort()).
+                setReceiverID(receiver.getId()).
+                setReceiverAddress(receiver.getAddress()).
+                setReceiverPort(receiver.getPort());
+    }
 }
