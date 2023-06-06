707b065af442bfc2928c80c8061d69ff543f24b4
freundp99 <a11919391@unet.univie.ac.at>
2023-06-06 21:41:54 +0200

    Exception for undefined strategy added

--- implementation/MSProsumer/src/main/java/Logic/AccountingStrategy/ContextCalcAcct.java
+++ implementation/MSProsumer/src/main/java/Logic/AccountingStrategy/ContextCalcAcct.java
@@ -2,19 +2,23 @@ package Logic.AccountingStrategy;
 
 import Data.IProsumerDevice;
 import Exceptions.DeviceNotSupportedException;
+import Exceptions.UndefinedStrategyException;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public class ContextCalcAcct {
-    private ICalAcctStrategy strategy;
+    private Optional<ICalAcctStrategy> strategy = Optional.empty();
 
     public void setCalcAcctAStrategy(ICalAcctStrategy concreteStrategy) {
-        this.strategy = concreteStrategy;
+        this.strategy = Optional.of(concreteStrategy);
     }
 
-    public double calculateAccounting(List<IProsumerDevice> devices, UUID timeSlotId) throws DeviceNotSupportedException {
-        return this.strategy.calculateAccounting(devices, timeSlotId);
+    public double calculateAccounting(List<IProsumerDevice> devices, UUID timeSlotId) throws DeviceNotSupportedException, UndefinedStrategyException {
+        if(this.strategy.isEmpty())
+            throw new UndefinedStrategyException();
+        return this.strategy.get().calculateAccounting(devices, timeSlotId);
 
     }
 }
