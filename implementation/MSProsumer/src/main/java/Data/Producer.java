057ede06596eee9ce371626d28edfde7c175fcd3
freundp99 <a11919391@unet.univie.ac.at>
2023-06-06 20:42:02 +0200

    Producer class changed to record class

--- implementation/MSProsumer/src/main/java/Data/Producer.java
+++ implementation/MSProsumer/src/main/java/Data/Producer.java
@@ -1,37 +1,11 @@
 package Data;
 
-public class Producer implements IProsumerDevice{
-
-    private final double area;
-
-    private final int efficiency;
-
-    private final int compassAngle;
-
-    private final int standingAngle;
-
-    public Producer(final double area, final int efficiency, final int compassAngle, final int standingAngle) {
-        this.area = area;
-        this.efficiency = efficiency;
-        this.compassAngle = compassAngle;
-        this.standingAngle = standingAngle;
-    }
-
-    public double getArea() {
-        return area;
-    }
-
-    public int getEfficiency() {
-        return efficiency;
-    }
-
-    public int getCompassAngle() {
-        return compassAngle;
-    }
-
-    public int getStandingAngle() {
-        return standingAngle;
-    }
+public record Producer(
+        double area,
+        int efficiency,
+        int compassAngle,
+        int standingAngle)
+        implements IProsumerDevice {
 
     @Override
     public <T> T getDevice() {
