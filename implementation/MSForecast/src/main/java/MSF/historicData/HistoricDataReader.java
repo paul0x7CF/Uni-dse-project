5dc0bf9a5783e45aad38bfc838b89904eef2f045
Zivan Rikanovic <a11827883@unet.univie.ac.at>
2023-06-12 12:14:37 +0200

    Added Historic Data

--- implementation/MSForecast/src/main/java/MSF/historicData/HistoricDataReader.java
+++ implementation/MSForecast/src/main/java/MSF/historicData/HistoricDataReader.java
@@ -1,10 +1,17 @@
 package MSF.historicData;
 
+import MSF.propertyHandler.PropertiesReader;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
  * Class to read the historic data from a CSV file.
  */
 public class HistoricDataReader {
-    public static HistoricData loadCSVFile() {
-        return null;
+    private static final Logger logger = LogManager.getLogger(HistoricDataReader.class);
+    private static final String HISTORIC_DATA_FILE_PATH = "implementation/MSForecast/src/main/resources/";
+    public static String readHistoricData() {
+        //TODO: read the historic data from the CSV file
+        return "DATA";
     }
 }
