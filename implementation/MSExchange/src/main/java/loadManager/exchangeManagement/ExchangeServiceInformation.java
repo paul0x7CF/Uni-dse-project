3ae3a4e5399ce7aab5bcbf6ccea414e9f1c59488
pflegerm01 <a12014173@unet.univie.ac.at>
2023-06-13 17:15:56 +0200

    added logging to ExchangeServiceInformation and deleted unnecessary variabls

--- implementation/MSExchange/src/main/java/loadManager/exchangeManagement/ExchangeServiceInformation.java
+++ implementation/MSExchange/src/main/java/loadManager/exchangeManagement/ExchangeServiceInformation.java
@@ -4,28 +4,17 @@ import java.util.UUID;
 
 public class ExchangeServiceInformation {
     private UUID exchangeId;
-    private String ipAddress;
-    private int port;
 
     private boolean atCapacity = false;
 
-    public ExchangeServiceInformation(UUID exchangeId, String ipAddress, int port) {
+    public ExchangeServiceInformation(UUID exchangeId) {
         this.exchangeId = exchangeId;
-        this.ipAddress = ipAddress;
-        this.port = port;
     }
 
     public UUID getExchangeId() {
         return exchangeId;
     }
 
-    public String getIpAddress() {
-        return ipAddress;
-    }
-
-    public int getPort() {
-        return port;
-    }
 
     public boolean isAtCapacity() {
         return atCapacity;
