300dd3ec9e1a0635516739a19903a1db81605f7a
freundp99 <a11919391@unet.univie.ac.at>
2023-06-13 22:31:43 +0200

    SolarResponse Message Changed

--- implementation/Communication/src/main/java/CF/sendable/SolarResponse.java
+++ implementation/Communication/src/main/java/CF/sendable/SolarResponse.java
@@ -4,10 +4,17 @@ import java.util.Map;
 import java.util.UUID;
 
 public class SolarResponse implements ISendable {
+
+    private final UUID requestTimeSlotId;
     private Map<UUID, Double> solarProduction;
 
-    public SolarResponse(Map<UUID, Double> solarProduction) {
+    public SolarResponse(UUID requestTimeSlotId, Map<UUID, Double> solarProduction) {
+        this.requestTimeSlotId = requestTimeSlotId;
         this.solarProduction = solarProduction;
+    }
+
+    public UUID getResponseTimeSlotId() {
+        return requestTimeSlotId;
     }
 
     public Map<UUID, Double> getSolarProduction() {
