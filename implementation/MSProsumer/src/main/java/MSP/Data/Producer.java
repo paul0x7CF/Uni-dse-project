f3deeeca8be8761b62bafc56e8bd8490d89a0c2b
freundp99 <a11919391@unet.univie.ac.at>
2023-06-14 13:53:00 +0200

    producer added to Config

--- implementation/MSProsumer/src/main/java/MSP/Data/Producer.java
+++ implementation/MSProsumer/src/main/java/MSP/Data/Producer.java
@@ -1,11 +1,33 @@
 package MSP.Data;
 
-public record Producer(
-        double area,
-        int efficiency,
-        int compassAngle,
-        int standingAngle)
-        implements IProsumerDevice {
+public class Producer implements IProsumerDevice {
+        private final double area;
+        private final int efficiency;
+        private final int compassAngle;
+        private final int standingAngle;
+
+    public Producer(double area, int efficiency, int compassAngle, int standingAngle) {
+        this.area = area;
+        this.efficiency = efficiency;
+        this.compassAngle = compassAngle;
+        this.standingAngle = standingAngle;
+    }
+
+    public double getArea() {
+        return area;
+    }
+
+    public int getEfficiency() {
+        return efficiency;
+    }
+
+    public int getCompassAngle() {
+        return compassAngle;
+    }
+
+    public int getStandingAngle() {
+        return standingAngle;
+    }
 
     @Override
     public <T> T getDevice() {
