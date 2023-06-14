11c49c6d259fcb910802c2bf099cf496617ad99d
freundp99 <a11919391@unet.univie.ac.at>
2023-06-15 00:22:58 +0200

    Poll Production extended for TimeOut

--- implementation/MSProsumer/src/main/java/MSP/Communication/polling/PollProductionForecast.java
+++ implementation/MSProsumer/src/main/java/MSP/Communication/polling/PollProductionForecast.java
@@ -1,12 +1,22 @@
 package MSP.Communication.polling;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class PollProductionForecast implements PollForecast {
 
+    private static final Logger logger = LogManager.getLogger(PollProductionForecast.class);
+
     private boolean isAvailable;
-    private double response;
+    private List<Double> response = new ArrayList<>();
+    private boolean atLeastOneResponse;
 
     public PollProductionForecast() {
         this.isAvailable = false;
+        this.atLeastOneResponse = false;
     }
 
     @Override
@@ -20,11 +30,34 @@ public class PollProductionForecast implements PollForecast {
 
     @Override
     public void setPollResult(Object response) {
-        this.response = (double) response;
+        this.response.add((Double) response);
+        if (!this.atLeastOneResponse) {
+            Thread timer = new Thread(this::setIsAvailableTrueAfterTime);
+            timer.start();
+            this.atLeastOneResponse = true;
+        }
+
     }
 
     @Override
     public Object getForecastResult() {
         return this.response;
+    }
+
+    public int getResponseSize() {
+        return this.response.size();
+    }
+
+    private void setIsAvailableTrueAfterTime() {
+        try {
+            Thread.sleep(5000);
+            if (!isAvailable){
+                logger.warn("Polling for ProductionForecast timed out and is now available because at least one response was received");
+                this.isAvailable = true;
+            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
