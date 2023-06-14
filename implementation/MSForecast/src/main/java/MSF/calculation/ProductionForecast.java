29e9cd57c1566af8963a1df488a288d2fe323078
Zivan Rikanovic <a11827883@unet.univie.ac.at>
2023-06-14 15:45:00 +0200

    Basic ProductionForecast started

--- implementation/MSForecast/src/main/java/MSF/calculation/ProductionForecast.java
+++ implementation/MSForecast/src/main/java/MSF/calculation/ProductionForecast.java
@@ -6,6 +6,8 @@ import MSF.data.EForecastType;
 import MSF.data.ProsumerConsumptionRequest;
 import CF.sendable.SolarRequest;
 import MSF.data.ProsumerSolarRequest;
+import MSF.exceptions.UnknownForecastTypeException;
+import MSF.historicData.HistoricDataReader;
 
 import java.util.concurrent.BlockingQueue;
 
@@ -27,15 +29,28 @@ public class ProductionForecast implements Runnable {
             try {
                 ProsumerSolarRequest prosumerSolarRequest = incomingSolarRequest.take();
                 predictProduction(prosumerSolarRequest);
-            } catch (InterruptedException e) {
+            } catch (InterruptedException | UnknownForecastTypeException e) {
                 e.printStackTrace();
             }
         }
     }
 
-    private void predictProduction(ProsumerSolarRequest prosumerSolarRequest) {
+    private void predictProduction(ProsumerSolarRequest prosumerSolarRequest) throws UnknownForecastTypeException {
 
         //TODO: calculate production (also check currentTimeSlotID)
+
+        double production = 0;
+
+        for (int i = 0; i < prosumerSolarRequest.getAmountOfPanels(); i++) {
+            switch (forecastType) {
+                case APOLIS -> {
+                    double irradiation = Double.parseDouble(HistoricDataReader.getHistoricData(currentTimeSlot, forecastType).split(";")[1]) * 1000 / 24;
+
+                    production += irradiation * prosumerSolarRequest.getArea()[i] * prosumerSolarRequest.getEfficiency()[i] * Math.cos(prosumerSolarRequest.getCompassAngle()[i]) * Math.cos(prosumerSolarRequest.getStandingAngle()[i]);
+                }
+            }
+        }
+
 
     }
 }
