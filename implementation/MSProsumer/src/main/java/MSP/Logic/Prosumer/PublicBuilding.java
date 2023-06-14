fc8cb4fc51b3ccf567b2c2304d27ae78ecf9c6a5
freundp99 <a11919391@unet.univie.ac.at>
2023-06-14 13:15:05 +0200

    Refactor Prosumer types

--- implementation/MSProsumer/src/main/java/MSP/Logic/Prosumer/PublicBuilding.java
+++ implementation/MSProsumer/src/main/java/MSP/Logic/Prosumer/PublicBuilding.java
@@ -2,7 +2,7 @@ package MSP.Logic.Prosumer;
 
 import MSP.Data.EProsumerType;
 
-public class PublicBuilding extends Prosumer {
+public class PublicBuilding extends ConsumptionBuilding {
     public PublicBuilding(EProsumerType prosumerType, double cashBalance, int port) {
         super(prosumerType, cashBalance, port);
     }
