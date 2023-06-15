b2c97c212dbc9ef8c806086d863db5ef560656c3
freundp99 <a11919391@unet.univie.ac.at>
2023-06-15 11:14:42 +0200

    TimeSlot constructor for Testing

--- implementation/Communication/src/main/java/CF/sendable/TimeSlot.java
+++ implementation/Communication/src/main/java/CF/sendable/TimeSlot.java
@@ -4,13 +4,21 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 public class TimeSlot implements ISendable {
-    private final UUID timeSlotID = UUID.randomUUID();
+    private final UUID timeSlotID;
     private LocalDateTime startTime;
     private LocalDateTime endTime;
 
     public TimeSlot(LocalDateTime startTime, LocalDateTime endTime) {
         this.startTime = startTime;
         this.endTime = endTime;
+        this.timeSlotID = UUID.randomUUID();
+    }
+
+    // Constructor for testing
+    public TimeSlot(LocalDateTime startTime, LocalDateTime endTime, UUID timeSlotID) {
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.timeSlotID = timeSlotID;
     }
 
     public UUID getTimeSlotID() {
