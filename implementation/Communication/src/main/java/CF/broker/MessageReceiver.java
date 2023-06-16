a22f9c96c75413fc0b6f4427aaa77cdc85d59f1a
Günther Dobler <a11911466@univie.ac.at>
2023-06-16 22:15:23 +0200

    Added definition of Maps in MessageReceiver in constructor. Timeout for Messages is now 10 times the ackTimeout from config.

--- implementation/Communication/src/main/java/CF/broker/MessageReceiver.java
+++ implementation/Communication/src/main/java/CF/broker/MessageReceiver.java
@@ -1,18 +1,32 @@
 package CF.broker;
 
+import CF.mainPackage.ConfigReader;
 import CF.protocol.Message;
 
 import java.time.Duration;
 import java.time.Instant;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 import java.util.UUID;
 
+/**
+ * This class is used to keep track of received messages and to check if a message has already been received.
+ * This is necessary because the broker may receive the same message multiple times due to resending of messages
+ * through the AckHandler. The MessageHandler should only process a message once.
+ */
 public class MessageReceiver {
-    private Map<UUID, Message> receivedMessages = new HashMap<>();
-    private Map<UUID, Instant> messageTimestamps = new HashMap<>();
-    private final Duration messageRetentionDuration = Duration.ofSeconds(60);
+    private final Map<UUID, Message> receivedMessages;
+    private final Map<UUID, Instant> messageTimestamps;
+    private final Duration messageRetentionDuration;
+
+    private MessageReceiver() {
+        receivedMessages = new HashMap<>();
+        messageTimestamps = new HashMap<>();
+
+        ConfigReader configReader = new ConfigReader();
+        int duration = Integer.parseInt(configReader.getProperty("ackTimeout")) * 10; // 10 times the ack timeout
+        messageRetentionDuration = Duration.ofSeconds(duration);
+    }
 
     public void receiveMessage(Message message) {
         UUID messageID = message.getMessageID();
