d764e0d21496808dbe905ea888bda7dbbfd23ed8
Günther Dobler <a11911466@univie.ac.at>
2023-06-17 22:55:07 +0200

    Javadoc in various classes.

--- implementation/Communication/src/main/java/CF/broker/discovery/IMessageSchedulerObserver.java
+++ implementation/Communication/src/main/java/CF/broker/discovery/IMessageSchedulerObserver.java
@@ -2,6 +2,10 @@ package CF.broker.discovery;
 
 import java.util.concurrent.ScheduledExecutorService;
 
+/**
+ * This interface is used to schedule messages.
+ * The MessageScheduler class will call the scheduleMessages method on all observers.
+ */
 public interface IMessageSchedulerObserver {
     void scheduleMessages(ScheduledExecutorService scheduler);
 }
--- implementation/Communication/src/main/java/CF/broker/discovery/IScheduleBroker.java
+++ implementation/Communication/src/main/java/CF/broker/discovery/IScheduleBroker.java
@@ -6,6 +6,11 @@ import CF.sendable.MSData;
 import java.util.List;
 import java.util.UUID;
 
+/**
+ * Interface for the ScheduleBroker. This interface is used to make provide the necessary methods for the
+ * {@link IMessageSchedulerObserver} implementations to schedule messages.
+ *
+ */
 public interface IScheduleBroker extends IBroker {
     MSData getCurrentService();
     List<MSData> getServices();
